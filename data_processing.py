439f7c0921a5949ae54a70860eb62554f8f8c6ee
Xiaoyang Liu <144244539+ERHUTUZI123@users.noreply.github.com>
2025-01-25 18:19:11 -0500

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -1,22 +1,19 @@
 import serial
-import time
 
-port = ''
+port = '/dev/cu.usbserial-10'
 baud_rate = 9600
 output_file = 'temperature_data.txt'
 
 def main():
     try:
-        with serial.Serial(port, baud_rate, timeout=1) as ser, open(output_file, 'a') as file:
+        with serial.Serial(port, baud_rate, timeout=0) as ser, open(output_file, 'a') as file:
             print("Reading data from Arduino...")
 
             while True:
                 if ser.in_waiting > 0:
                     line = ser.readline().decode('utf-8').strip()
                     print(f"Received: {line}")
-                    timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
-                    file.write(f"{timestamp}, {line}\n")
-
+                    file.write(line + '\n')
                     file.flush()
 
     except KeyboardInterrupt:
