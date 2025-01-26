85c71f5a56387ccaa7296eca02e174de39191c4e
Xiaoyang Liu <144244539+ERHUTUZI123@users.noreply.github.com>
2025-01-26 10:08:54 -0500

    Upload backup files

---
+++ backup_react/api.py
@@ -0,0 +1,74 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+import os
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+temperature_file = 'temperature_data.txt'
+humidity_file = 'humidity_data.txt'
+
+def get_latest_temperature():
+    try:
+        if os.path.exists(temperature_file):
+            with open(temperature_file, 'r') as file:
+                lines = file.readlines()
+
+            if lines:
+                latest_line = lines[-1].strip()  # Get the last line and strip any whitespace
+                return latest_line  # Return the temperature value directly
+            else:
+                return None  # File is empty
+        else:
+            return None  # File does not exist
+    except Exception as e:
+        print(f"Error reading file: {str(e)}")
+        return None
+    
+def get_latest_humidity():
+    try:
+        if os.path.exists(humidity_file):
+            with open(humidity_file, 'r') as file:
+                lines = file.readlines()
+
+            if lines:
+                latest_line = lines[-1].strip()  # Get the last line and strip any whitespace
+                return latest_line  # Return the temperature value directly
+            else:
+                return None  # File is empty
+        else:
+            return None  # File does not exist
+    except Exception as e:
+        print(f"Error reading humidity file: {str(e)}")
+        return None
+
+
+@app.get("/temperature")
+def get_temperature():
+    try:
+        latest_temp = get_latest_temperature()
+        if latest_temp:
+            return {"temperature": latest_temp}
+        else:
+            return {"temperature": "No data found"}
+    except Exception as e:
+        return {"error": str(e)}
+    
+@app.get("/humidity")
+def get_humidity():
+    try:
+        latest_hum = get_latest_humidity()
+        if latest_hum:
+            return {"humidity": latest_hum}
+        else:
+            return {"humidity": "No data found"}
+    except Exception as e:
+        return {"error": str(e)}
