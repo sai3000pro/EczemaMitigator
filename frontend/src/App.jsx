26f4ecdc52bf8d41d0f15ff839220d39b8c6573e
sai3000pro <96095408+sai3000pro@users.noreply.github.com>
2025-01-25 23:34:48 -0500

    broken navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,25 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() { 
-  // Delete if not needed
-  const [count, setCount] = useState(0);
+function App() {
   // State to store data from API
   const [data, setData] = useState(null);
   // Fetch data from API
 
   // Path: backend/controllers/tempController.js
   useEffect(() => {
-    axios.get('http://localhost:5005/api/temperature')
-      .then(response => {
+    axios
+      .get("http://localhost:5005/api/temperature")
+      .then((response) => {
         setData(response.data);
       })
-      .catch(error => {
-        console.error('Error fetching data:', error);
+      .catch((error) => {
+        console.error("Error fetching data:", error);
       });
   }, []);
-
 
   // bulk of the code
 
@@ -35,17 +33,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      
+
       <h1>Data from API:</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        {data && <div>{JSON.stringify(data)}</div>}
-      </div>
+      <div className="card">{data && <div>{JSON.stringify(data)}</div>}</div>
     </div>
   );
 }
